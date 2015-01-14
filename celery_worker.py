f497c2d9c97b9d0c5ce3f109c567ab357494cd12
Beau Gunderson <beau@beaugunderson.com>
2015-01-13 17:14:49 -0800

    SSLify our app, cleanup
    - separate celery instantation into its own module

---
+++ celery_worker.py
@@ -0,0 +1,20 @@
+from celery import Celery
+
+
+def make_worker(app):
+    """Set up celery tasks for an app."""
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+
+    return celery
--- client.py
+++ client.py
@@ -3,109 +3,112 @@
 
 import os
 
-from celery import Celery
 from celery.signals import task_postrun
-import flask
-from flask import request
+
+from flask import Flask, request
+from flask_sslify import SSLify
+
 import requests
 
 from data_retrieval.american_gut import create_amgut_ohdataset
 from data_retrieval.twenty_three_and_me import create_23andme_ohdataset
 
-PORT = 5000
+from celery_worker import make_worker
 
+PORT = os.getenv('PORT', 5000)
 
-#####################################################################
-# Set up celery and tasks.
-def make_celery(app):
-    """Set up celery tasks for an app."""
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
+ohdata_app = Flask(__name__)
 
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-ohdata_app = flask.Flask("client")
 ohdata_app.config.update(
+    DEBUG=os.getenv('DEBUG', False),
     CELERY_BROKER_URL=os.environ.get('CLOUDAMQP_URL', 'amqp://'),
-    BROKER_POOL_LIMIT=0,
-)
-celery_worker = make_celery(ohdata_app)
+    BROKER_POOL_LIMIT=0)
+
+sslify = SSLify(ohdata_app)
+
+celery_worker = make_worker(ohdata_app)
 
 
 @task_postrun.connect()
-def task_postrun_handler(sender=None, state=None, kwargs=None, **other_kwargs):
-    params = {'name': sender.name,
-              'state': state,
-              's3_key_name': kwargs['s3_key_name']}
+def task_postrun_handler_cb(sender=None, state=None, kwargs=None,
+                            **other_kwargs):
+    params = {
+        'name': sender.name,
+        'state': state,
+        's3_key_name': kwargs['s3_key_name']
+    }
+
     url = kwargs['update_url']
+
     requests.post(url, data=params)
 
 
+# Celery tasks
 @celery_worker.task()
-def make_amgut_ohdataset(barcode, s3_key_name, s3_bucket_name):
-    """Task to initiate retrieval of American Gut data set"""
+def make_amgut_ohdataset(barcode, s3_key_name, s3_bucket_name, update_url):
+    """
+    Task to initiate retrieval of American Gut data set
+    """
+    print "Starting work on American Gut dataset"
+
     create_amgut_ohdataset(barcode=barcode,
-                           s3_bucket_name=S3_bucket_name,
+                           s3_bucket_name=s3_bucket_name,
                            s3_key_name=s3_key_name,
-                           update_url=update_url,
-                           )
+                           update_url=update_url)
 
 
 @celery_worker.task()
-def make_23andme_ohdataset(access_token, profile_id,
-                           s3_key_name, s3_bucket_name):
-    """Task to initiate retrieval of 23andme data set"""
-    print "Working starting 23andme taskher..."
+def make_23andme_ohdataset(access_token, profile_id, s3_key_name,
+                           s3_bucket_name, update_url):
+    """
+    Task to initiate retrieval of 23andme data set
+    """
+    print "Starting work on 23andMe dataset"
+
     create_23andme_ohdataset(access_token=access_token,
                              profile_id=profile_id,
                              s3_bucket_name=s3_bucket_name,
                              s3_key_name=s3_key_name,
-                             update_url=update_url,
-                             )
+                             update_url=update_url)
 
 
-#####################################################################
-# Pages to receive task requests.
+# Pages to receive task requests
 @ohdata_app.route('/23andme', methods=['GET', 'POST'])
 def twenty_three_and_me():
-    """Page to receive 23andme task request"""
-    print "Starting 23andme dataset..."
-    # if request.method == 'POST':
+    """
+    Page to receive 23andme task request
+    """
     make_23andme_ohdataset.delay(access_token=request.args['access_token'],
                                  profile_id=request.args['profile_id'],
                                  s3_key_name=request.args['s3_key_name'],
                                  s3_bucket_name=request.args['s3_bucket_name'],
-                                 update_url=request.args['update_url'],
-                                 )
+                                 update_url=request.args['update_url'])
+
     return "23andme dataset started"
 
 
 @ohdata_app.route('/amgut', methods=['GET', 'POST'])
 def american_gut():
-    """Page to receive American Gut task request"""
-    # if request.method == 'POST':
+    """
+    Page to receive American Gut task request
+    """
     make_amgut_ohdataset.delay(barcode=request.args['barcode'],
                                s3_key_name=request.args['s3_key_name'],
                                s3_bucket_name=request.args['s3_bucket_name'],
-                               update_url=request.args['update_url'],
-                               )
+                               update_url=request.args['update_url'])
+
     return "Amgut dataset started"
 
 
 @ohdata_app.route('/', methods=['GET', 'POST'])
 def main_page():
-    """Main page for the app."""
-    return "Open Humans Data Extraction - our Flask app"
+    """
+    Main page for the app.
+    """
+    return "Open Humans Data Processing"
 
 
 if __name__ == '__main__':
-    print "A local client for Open Humans data extraction is now initialized."
+    print "A local client for Open Humans data processing is now initialized."
+
     ohdata_app.run(debug=True, port=PORT)
