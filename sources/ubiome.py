f9e1a76a766cd21308bc2ce29badfdf6428a56c1
Beau Gunderson <beau@beaugunderson.com>
2016-08-09 14:06:52 -0700

    convert ubiome

--- sources/ubiome.py
+++ sources/ubiome.py
@@ -15,131 +15,96 @@ Will assemble processed data sets in files/
 
 import os
 import shutil
-import tempfile
 import zipfile
 
 from cStringIO import StringIO
 
-import requests
-
-from data_retrieval.files import get_remote_file, mv_tempfile_to_output
+from base_source import BaseSource
 
 
-def verify_ubiome(input_filepath, sentry=None, username=None):
-    """
-    Verify that this is a uBiome file.
-    """
-    if input_filepath.endswith('.zip'):
-        zip_file = zipfile.ZipFile(input_filepath)
-
-        file_list = [f for f in zip_file.namelist()
-                     if not f.startswith('__MACOSX/')]
-
-        for filename in file_list:
-            if not filename.endswith('.fastq.gz'):
-                if sentry:
-                    sentry_msg = ('uBiome file did not conform to expected '
-                                  'format.')
-
-                    if username:
-                        sentry_msg += ' Username: {}'.format(username)
-
-                    sentry.captureMessage(sentry_msg)
-
-                raise ValueError(
-                    'Found a filename that did not end with ".fastq.gz": '
-                    '"{}"'.format(filename))
-    else:
-        raise ValueError('Input file is expected to be a ZIP archive')
-
-
-def create_datafiles(username, samples=None, task_id=None, update_url=None,
-                     sentry=None, **kwargs):
+class UBiomeSource(BaseSource):
     """
     Create Open Humans Dataset from uploaded uBiome data.
 
     Optional arguments:
         samples: path to an online copy of the input file
-        filedir: Local filepath, folder in which to place the resulting file.
-        s3_bucket_name: S3 bucket to write resulting file.
-        s3_key_dir: S3 key "directory" to write resulting file. The full S3 key
-                    name will add a filename to the end of s3_key_dir.
-
-    For output: iither 'filedir' (and no S3 arguments), or both
-    's3_bucket_name' and 's3_key_dir' (and no 'filedir') must be specified.
     """
-    tempdir = tempfile.mkdtemp()
-    temp_files = []
-    data_files = []
 
-    if not samples:
-        raise Exception('`samples` parameter missing')
+    def __init__(self, username, samples, **kwargs):
+        self.username = username
+        self.samples = samples
 
-    for sample in enumerate(samples):
-        filename = get_remote_file(sample[1]['sequence_file']['url'], tempdir)
-        input_file = os.path.join(tempdir, filename)
+        super(UBiomeSource, self).__init__(**kwargs)
 
-        verify_ubiome(input_file, sentry, username)
+    def verify_ubiome(self, input_filepath):
+        """
+        Verify that this is a uBiome file.
+        """
+        if input_filepath.endswith('.zip'):
+            zip_file = zipfile.ZipFile(input_filepath)
 
-        fastq_filename = 'uBiome-fastq{}.zip'.format(
-            '-' + str(sample[0] + 1) if len(samples) > 1 else '')
-        shutil.move(input_file, os.path.join(tempdir, fastq_filename))
-        metadata = {
-            'description': 'uBiome 16S FASTQ raw sequencing data.',
-            'tags': ['fastq', 'uBiome', '16S']
-        }
-        if sample[1]['additional_notes']:
-            metadata['user_notes'] = sample[1]['additional_notes']
-        temp_files.append({
-            'temp_filename': fastq_filename,
-            'tempdir': tempdir,
-            'metadata': metadata,
-        })
+            file_list = [f for f in zip_file.namelist()
+                         if not f.startswith('__MACOSX/')]
 
-        taxonomy = StringIO(sample[1]['taxonomy'])
-        taxonomy_filename = 'taxonomy{}.json'.format(
-            '-' + str(sample[0] + 1) if len(samples) > 1 else '')
-        shutil.copyfileobj(taxonomy,
-                           file(os.path.join(tempdir, taxonomy_filename), 'w'))
-        metadata = {
-            'description': 'uBiome 16S taxonomy data, JSON format.',
-            'tags': ['json', 'uBiome', '16S']
-        }
-        if sample[1]['additional_notes']:
-            metadata['user_notes'] = sample[1]['additional_notes']
-        temp_files.append({
-            'temp_filename': taxonomy_filename,
-            'tempdir': tempdir,
-            'metadata': metadata,
-        })
+            for filename in file_list:
+                if not filename.endswith('.fastq.gz'):
+                    self.sentry_log('uBiome file did not conform to '
+                                    'expected format. Username: {}'.format(
+                                        self.username))
 
-    print 'Finished creating all datasets locally.'
+                    raise ValueError(
+                        'Found a filename that did not end with ".fastq.gz": '
+                        '"{}"'.format(filename))
+        else:
+            raise ValueError('Input file is expected to be a ZIP archive')
 
-    for file_info in temp_files:
-        print 'File info: {}'.format(str(file_info))
+    def create_datafiles(self):
+        for sample in enumerate(self.samples):
+            filename = self.get_remote_file(sample[1]['sequence_file']['url'])
+            input_file = os.path.join(self.temp_directory, filename)
 
-        filename = file_info['temp_filename']
-        file_tempdir = file_info['tempdir']
+            self.verify_ubiome(input_file)
 
-        output_path = mv_tempfile_to_output(
-            os.path.join(file_tempdir, filename), filename, **kwargs)
+            fastq_filename = 'uBiome-fastq{}.zip'.format(
+                '-' + str(sample[0] + 1) if len(self.samples) > 1 else '')
 
-        if 's3_key_dir' in kwargs and 's3_bucket_name' in kwargs:
-            data_files.append({
-                's3_key': output_path,
-                'metadata': file_info['metadata'],
+            shutil.move(input_file,
+                        os.path.join(self.temp_directory, fastq_filename))
+
+            metadata = {
+                'description': 'uBiome 16S FASTQ raw sequencing data.',
+                'tags': ['fastq', 'uBiome', '16S']
+            }
+
+            if sample[1]['additional_notes']:
+                metadata['user_notes'] = sample[1]['additional_notes']
+
+            self.temp_files.append({
+                'temp_filename': fastq_filename,
+                'metadata': metadata,
             })
 
-    print 'Finished moving all datasets to permanent storage.'
+            taxonomy = StringIO(sample[1]['taxonomy'])
+            taxonomy_filename = 'taxonomy{}.json'.format(
+                '-' + str(sample[0] + 1) if len(self.samples) > 1 else '')
 
-    os.rmdir(tempdir)
+            shutil.copyfileobj(
+                taxonomy,
+                file(os.path.join(self.temp_directory, taxonomy_filename),
+                     'w'))
 
-    if not (task_id and update_url):
-        return
+            metadata = {
+                'description': 'uBiome 16S taxonomy data, JSON format.',
+                'tags': ['json', 'uBiome', '16S']
+            }
 
-    task_data = {'task_id': task_id, 'data_files': data_files}
+            if sample[1]['additional_notes']:
+                metadata['user_notes'] = sample[1]['additional_notes']
 
-    requests.post(update_url, json={'task_data': task_data})
+            self.temp_files.append({
+                'temp_filename': taxonomy_filename,
+                'metadata': metadata,
+            })
 
 
 # if __name__ == '__main__':
